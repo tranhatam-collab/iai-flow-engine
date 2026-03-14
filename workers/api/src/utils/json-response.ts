55922aaa0b9568f37956f0d6954cea0ffb56465b
tranhatam-collab <tranhatam@gmail.com>
2026-03-14 14:23:41 +0700

    Create json-response.ts

---
+++ workers/api/src/utils/json-response.ts
@@ -0,0 +1,9 @@
+export function json(data: unknown, status = 200) {
+  return new Response(JSON.stringify(data), {
+    status,
+    headers: {
+      "content-type": "application/json; charset=utf-8",
+      "access-control-allow-origin": "*"
+    }
+  })
+}
